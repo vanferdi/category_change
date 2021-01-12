0db89cee87fd41b851a705e260813fc8f6c729f8
Vanessa Ferdinand <vanessa.ferdinand@unimelb.edu.au>
2021-01-12 16:36:04 +1100

    added basic data descriptives and updated convergence rates file

--- Analyses/convergence_rates.R
+++ Analyses/convergence_rates.R
@@ -1,54 +1,90 @@
 df1 <- read.csv("../Data/experiment1_FINAL.csv")
+df2 <- read.csv("../Data/experiment2_FINAL.csv")
 
 df1c <- subset(df1,condition=="C")
 df1i <- subset(df1,condition=="I")
+df2c <- subset(df2,condition=="C")
+df2i <- subset(df2,condition=="I")
 
 # number of trajectories that haven't converged yet, per iteration
 table(df1c$iteration)
 table(df1i$iteration)
+table(df2c$iteration)
+table(df2i$iteration)
 
 #############################################################################
 # get percentage of trajectories that converged on each iteration
 
 # cultural condition
+
 x <- c(as.vector(table(df1c$iteration)[2:11]),0) # add zero coz we know it converged
 c1 <- 1-(x/45) # make inverse into a percentage
 
+x <- c(as.vector(table(df2c$iteration)[2:15]),0) 
+c2 <- 1-(x/45)
+
 # individual condition
+
 # need to look up how many trajectories in the last iteration were and weren't converged
-table(subset(df1i,iteration==8)$converged)
-# 8 converged and 11 did not
+table(subset(df1i,iteration==8)$converged) # 8 converged and 11 did not
 x <- c(as.vector(table(df1i$iteration)[2:8]),11) # so add 11
 i1 <- 1-(x/90) # make inverse into a percentage
 i1 <- c(i1,"NA","NA","NA")
 
+table(subset(df2i,iteration==8)$converged) # 10 converged and 16 did not
+x <- c(as.vector(table(df2i$iteration)[2:8]),16) # so add 16
+i2 <- 1-(x/90) 
+i2 <- c(i2,"NA","NA","NA","NA","NA","NA","NA")
+
 #############################################################################
 
+# Experiment 1
 
-plot(c1,type="l",las=1, xaxt="n",xlab="iteration",ylab="converged trajectories")
+dev.new(width=440,height=400, unit="px")
+plot(c1,type="l",las=1, xaxt="n",xlab="iteration",ylab="converged",main="Experiment 1")
 rect(par("usr")[1],par("usr")[3],par("usr")[2],par("usr")[4],col="#DCDCDC")
 abline(v=seq(1,11),h=seq(0,1,by=0.1),col="white")
-lines(i1,type="l",las=1)
-lines(c1,type="l",las=1)
-points(c1,pch=1) # 16
-points(i1,pch=2) # 17
-# make your own x axis labels
-xtick < -seq(1, 11, by=1)
-axis(side=1, at=xtick, labels=FALSE)
-text(x=xtick, par("usr")[3], labels=xtick, pos=1, offset=1, xpd=TRUE)
-
-
-
-
-plot(c1,type="l",las=1, xaxt="n",xlab="iteration",ylab="converged trajectories")
-rect(par("usr")[1],par("usr")[3],par("usr")[2],par("usr")[4],col="#DCDCDC")
-abline(v=seq(1,11),h=seq(0,1,by=0.1),col="white")
-lines(i1,type="l",las=1,lwd=2,lty="dashed")
+lines(i1,type="l",las=1,lwd=2,lty="twodash")
 lines(c1,type="l",las=1,lwd=2)
 # make your own x axis labels
-xtick < -seq(1, 11, by=1)
+xtick <- seq(1, 11, by=1)
 axis(side=1, at=xtick, labels=FALSE)
 text(x=xtick, par("usr")[3], labels=xtick, pos=1, offset=1, xpd=TRUE)
 
+# Experiment 2
 
+dev.new(width=600,height=400, unit="px")
+plot(c2,type="l",las=1, xaxt="n",xlab="iteration",ylab="",main="Experiment 2")
+rect(par("usr")[1],par("usr")[3],par("usr")[2],par("usr")[4],col="#DCDCDC")
+abline(v=seq(1,15),h=seq(0,1,by=0.1),col="white")
+lines(i2,type="l",las=1,lwd=2,lty="twodash")
+lines(c2,type="l",las=1,lwd=2)
+# make your own x axis labels
+xtick <- seq(1, 15, by=1)
+axis(side=1, at=xtick, labels=FALSE)
+text(x=xtick, par("usr")[3], labels=xtick, pos=1, offset=1, xpd=TRUE)
+legend(10, 0.5, legend=c("cultural", "individual"), lty=c("solid","twodash"), cex=1)
+
+
+#############################################################################
+# sanity check something
+
+# print the number of test systems that equalled the train system
+# (in iteration 1, the train system is a random initial system)
+
+get_result <- function(d) {
+	result <- c()
+	for (i in 1:max(d$iteration)) {
+		#print(paste("iteration",i))
+		sub <- subset(d,iteration==i)
+		#print(as.integer(table(sub$converged)["TRUE"]))
+		result <- c(result,as.integer(table(sub$converged)["TRUE"]))
+	}
+	return(result)
+}
+
+result_1c <- get_result(df1c)
+result_1i <- get_result(df1i)
+result_2c <- get_result(df2c)
+result_2i <- get_result(df2i)
 
---
+++ Analyses/data_orientation.R
@@ -0,0 +1,72 @@
+# this file contains basic info about the data and descriptives
+
+df1 <- read.csv("../Data/experiment1_FINAL.csv")
+df2 <- read.csv("../Data/experiment2_FINAL.csv")
+
+
+# number of participants
+length(unique(df1$participant))     # 297 in experiment 1
+length(unique(df2$participant))     # 313 in experiment 2
+
+# participants per condition
+df1c <- subset(df1,condition=="C")
+df1i <- subset(df1,condition=="I")
+length(unique(df1c$participant))    # 207 in cultural
+length(unique(df1i$participant))    # 90 in individual
+
+df2c <- subset(df2,condition=="C")
+df2i <- subset(df2,condition=="I")
+length(unique(df2c$participant))    # 223 in cultural
+length(unique(df2i$participant))    # 90 in individual
+
+# number of category systems
+length(df1$system512)               # 642 in experiment 1
+length(df2$system512)               # 692 in experiment 2
+
+# number of trajectories
+length(unique(df1c$trajectory))     # 45 in Exp 1 C
+length(unique(df1i$trajectory))     # 90 in Exp 1 I
+length(unique(df2c$trajectory))     # 45 in Exp 2 C
+length(unique(df2i$trajectory))     # 90 in Exp 2 I
+sum(45,45,90,90)                    # 270 total
+
+#########################################################################
+# duration of the experiment in minutes
+
+# average
+mean(df1c$duration_experiment)/60000 # 6.820537   6 min 49 sec
+mean(df1i$duration_experiment)/60000 # 17.2528   17 min 15 sec
+mean(df2c$duration_experiment)/60000 # 6.194271   6 min 12 sec
+mean(df2i$duration_experiment)/60000 # 17.68063  17 min 41 sec
+
+0.820537*60
+0.2528*60
+0.194271*60
+0.68063*60
+
+# max
+max(df1c$duration_experiment)/60000 # 40.58247
+max(df1i$duration_experiment)/60000 # 51.39727
+max(df2c$duration_experiment)/60000 # 25.12885
+max(df2i$duration_experiment)/60000 # 72.14815 (outlier)
+
+# min
+min(df1c$duration_experiment)/60000 # 4.01455
+min(df1i$duration_experiment)/60000 # 3.922617
+min(df2c$duration_experiment)/60000 # 3.911183
+min(df2i$duration_experiment)/60000 # 4.045917
+
+table(df1c$duration_experiment/60000)
+table(df1i$duration_experiment/60000)
+table(df2c$duration_experiment/60000)
+table(df2i$duration_experiment/60000)
+
+# btw I couldn't use df2$whole_experiment coz there was an error in line 692
+subset(df2,whole_experiment=="whole_experiment")
+# here's the fix (floored minutes from 372715/60000)
+df2[692,]$whole_experiment <- 6
+
+# 8 rounds of the I condition experiment was designed to take less than 1 hours
+# so participants who took over an hour must have taken a break.
+
+#########################################################################
