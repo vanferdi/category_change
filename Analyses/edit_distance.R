5b791fa5c8a52f9684b6252a92f174542716061c
Vanessa Ferdinand <vanessa.ferdinand@unimelb.edu.au>
2021-01-14 12:42:16 +1100

    transition matrix between category system types

--- Analyses/edit_distance.R
+++ Analyses/edit_distance.R
@@ -1,3 +1,13 @@
+df1 <- read.csv("/Users/vanferdi/Library/Mobile Documents/com~apple~CloudDocs/Research/PROJECTS/Category Change/GITHUB REPO/Data/experiment1_FINAL.csv")
+df2 <- read.csv("/Users/vanferdi/Library/Mobile Documents/com~apple~CloudDocs/Research/PROJECTS/Category Change/GITHUB REPO/Data/experiment2_FINAL.csv")
+
+df1c <- subset(df1,condition=="C")
+df1i <- subset(df1,condition=="I")
+df2c <- subset(df2,condition=="C")
+df2i <- subset(df2,condition=="I")
+
+
+
 a <- "1111100000"
 b <- "1011100000" # 1 change
 c <- "0111100001" # 2 changes
@@ -66,3 +76,69 @@ table(subset(df1,edits==0)$system512,subset(df1,edits==0)$edits)
 table(df1$N_boundaries,df1$edits)
 
 # TO DO - plot the table above nicely
+
+
+##########################################################################
+# what are the TPs among N_boundaries?
+
+table(df1$N_boundaries_input,df1$N_boundaries)
+" in is rows, out is on top"
+"   1   2   3   4   5   6   7   8
+1 177   0  11   2   1   0   0   0
+2  12  78   8   8   0   0   0   0
+3  46   1  42   4   7   2   1   1
+4   3  37   7  28   2   7   1   1
+5  20   4  18   4  25   0   3   0
+6   8  10   7  16   3   5   0   0
+7   8   2   6   3   2   2   1   0
+8   0   1   1   4   0   0   2   0"
+
+systype_input <- if(df1$N_boundaries_input==1){"continuous"}
+
+ifelse(df1$N_boundaries_input==1, "continous", "disjoint")
+
+systype_input <- ifelse(df2$N_boundaries_input==0, "degenerate", ifelse(df2$N_boundaries_input==1, "continous", "disjoint"))
+cbind(df2$N_boundaries_input,x)
+
+##########################################################################
+# what are the TPs among continuous and disjoint systems?
+
+# add to df a column that categorizes input and output systems into 3 types
+systype_input <- ifelse(df1$N_boundaries_input==0, "degenerate", ifelse(df1$N_boundaries_input==1, "continous", "disjoint"))
+systype_output <- ifelse(df1$N_boundaries==0, "degenerate", ifelse(df1$N_boundaries==1, "continous", "disjoint"))
+df1 <- cbind(df1,systype_input,systype_output)
+
+systype_input <- ifelse(df2$N_boundaries_input==0, "degenerate", ifelse(df2$N_boundaries_input==1, "continous", "disjoint"))
+systype_output <- ifelse(df2$N_boundaries==0, "degenerate", ifelse(df2$N_boundaries==1, "continous", "disjoint"))
+df2 <- cbind(df2,systype_input,systype_output)
+
+a <- table(df1$systype_input,df1$systype_output)
+"         continous disjoint
+continous       177       14
+disjoint         97      354
+"
+
+b <- table(df2$systype_input,df2$systype_output)
+"          continous degenerate disjoint
+continous        186          4       43
+degenerate         2         14        3
+disjoint         134         15      291
+"
+
+all_ins <- unlist(list(df1$systype_input,df2$systype_input))
+all_outs <- unlist(list(df1$systype_output,df2$systype_output))
+
+all <- table(all_ins,all_outs)
+
+# normalize to each row sums to one
+pall <- all/rowSums(all)
+
+# get stationary distribution
+stat_dist <- function(matrix) { # returns the stationary distribution of a RIGHT stochastic matrix (rows sum to one)
+	vec <- eigen(t(matrix))$vectors[,1]
+	return(vec/sum(vec))
+}
+stat_dist(pall) # 0.6264775 0.3299556 0.0435669
+
+
+
