6b0dc0ad1e7fb243fd132b3d4ecc6c45761c9e43
Vanessa Ferdinand <vanessa.ferdinand@unimelb.edu.au>
2021-03-05 16:13:20 +1100

    added expansion rate stuff

--- Analyses/discovery_rates.R
+++ Analyses/discovery_rates.R
@@ -1,6 +1,7 @@
-df1 <- read.csv("/Users/vanferdi/Library/Mobile Documents/com~apple~CloudDocs/Research/PROJECTS/Category Change/GITHUB REPO/Data/experiment1_FINAL.csv")
-df2 <- read.csv("/Users/vanferdi/Library/Mobile Documents/com~apple~CloudDocs/Research/PROJECTS/Category Change/GITHUB REPO/Data/experiment2_FINAL.csv")
 
+df1 <- read.csv("/Users/vanferdi/Library/Mobile Documents/com~apple~CloudDocs/Research/PROJECTS/Category Change/GITHUB REPO/Data/experiment1_FINAL.csv", colClasses=c(system1024="character"))
+df2 <- read.csv("/Users/vanferdi/Library/Mobile Documents/com~apple~CloudDocs/Research/PROJECTS/Category Change/GITHUB REPO/Data/experiment2_FINAL.csv", colClasses=c(system1024="character"))
+	
 ##########################################################################
 # Discovery Rate
 ##########################################################################
@@ -12,6 +13,7 @@ df2 <- read.csv("/Users/vanferdi/Library/Mobile Documents/com~apple~CloudDocs/Re
 length(unique(df1$system512))/nrow(df1)    # 188 / 642 = 0.293 - the probability that each try results in discovery of a new system
 length(unique(df1i$system512))/nrow(df1i)  # 159 / 435 = 0.366
 length(unique(df1c$system512))/nrow(df1c)  #  75 / 207 = 0.362
+
 # Experiment 2
 length(unique(df2$system512))/nrow(df2)    # 183 / 692 = 0.264 - interesting that the total % is lower
 length(unique(df2i$system512))/nrow(df2i)  # 151 / 469 = 0.322 - means they had diff exploratory niches?
@@ -84,148 +86,178 @@ length(unique(c(df1c$system512,df1c$system512_input))) # 106
 # each row is a try - the init systems weren't the product of a try
 # I think don't include the initial sets as discoveries
 
-##########################
+##########################################################################
 # Discovery rate over time
+##########################################################################
 
 # get the cumulatively unique systems per iteration - ignore the init systems coz they weren't "discovered"
 
-# Experiment 1, condition I
-df <- df1i
-total_tries <- table(df$iteration)  # number of tries per iteration, 
-" 1  2  3  4  5  6  7  8 
-90 90 78 63 41 32 22 19"
-u1 <- unique(subset(df,iteration==1)$system512)
-u2 <- unique(subset(df,iteration==2)$system512)
-u3 <- unique(subset(df,iteration==3)$system512)
-u4 <- unique(subset(df,iteration==4)$system512)
-u5 <- unique(subset(df,iteration==5)$system512)
-u6 <- unique(subset(df,iteration==6)$system512)
-u7 <- unique(subset(df,iteration==7)$system512)
-u8 <- unique(subset(df,iteration==8)$system512)
+# gets the set of unique categories per iteration
+itersets <- function(df_name,system_type) {
+	sets <- c()  # the set of unique systems per iteration
+	setsize <- 0 # the number of unique sysetms per iteration
+	for (i in 1:max(df_name$iteration)) {
+		if (system_type == 512) { u <- unique(subset(df_name,iteration==i)$system512) }
+		if (system_type == 1024) { u <- unique(subset(df_name,iteration==i)$system1024) }
+		sets <- c(sets,list(u))
+	}
+	setsize <- lengths(sets) # the number of unique sysetms per iteration
+	return(sets)
+}
 
-dr1 <- length(u1)/total_tries[1]
-dr2 <- length(setdiff(u2,u1))/total_tries[2] # setdiff(a,b) returns what is in a and not in b
-dr3 <- length(setdiff(u3,c(u2,u1)))/total_tries[3]
-dr4 <- length(setdiff(u4,c(u3,u2,u1)))/total_tries[4]
-dr5 <- length(setdiff(u5,c(u4,u3,u2,u1)))/total_tries[5]
-dr6 <- length(setdiff(u6,c(u5,u4,u3,u2,u1)))/total_tries[6]
-dr7 <- length(setdiff(u7,c(u6,u5,u4,u3,u2,u1)))/total_tries[7]
-dr8 <- length(setdiff(u8,c(u7,u6,u5,u4,u3,u2,u1)))/total_tries[8]
+# gets the total number of categories systems per iteration
+itertries <- function(df_name,system_type) {
+	tries <- c() # total number of tries per iteration
+	for (i in 1:max(df_name$iteration)) {
+		tries <- c(tries,nrow(subset(df_name,iteration==i)))
+	}
+	return(tries)
+}
 
-e1i_rates <- c(dr1,dr2,dr3,dr4,dr5,dr6,dr7,dr8)
+# example usage
+sets <- itersets(df1i,512)
+tries <- itertries(df1i,512)
+setsize <- lengths(sets)
+sets[[1]] # the set of category systems in iteration 1
 
-# Experiment 1, condition C
-df <- df1c
-total_tries <- table(df$iteration)  # number of tries per iteration, 
-" 1  2  3  4  5  6  7  8  9 10 11 
-45 45 32 24 20 14 11  7  5  3  1 "
-u1 <- unique(subset(df,iteration==1)$system512)
-u2 <- unique(subset(df,iteration==2)$system512)
-u3 <- unique(subset(df,iteration==3)$system512)
-u4 <- unique(subset(df,iteration==4)$system512)
-u5 <- unique(subset(df,iteration==5)$system512)
-u6 <- unique(subset(df,iteration==6)$system512)
-u7 <- unique(subset(df,iteration==7)$system512)
-u8 <- unique(subset(df,iteration==8)$system512)
-u9 <- unique(subset(df,iteration==9)$system512)
-u10 <- unique(subset(df,iteration==10)$system512)
-u11 <- unique(subset(df,iteration==11)$system512)
+# returns the set of systems discovered for the first time per iteration
+cumulative_novel_counts <- function(itersets_result) {
+	new_discoveries <- list(unique(itersets_result[[1]])) # call all of the sets in the first iteration new discoveries
+	for (i in 2:length(itersets_result)) { # for each iteration, 2 to max
+		all_previous <- unlist(itersets_result[1:(i-1)])
+		current_set <- unlist(itersets_result[i])
+		n <- setdiff(current_set,all_previous) # returns what is in current_set and not in all_previous
+		new_discoveries <- c(new_discoveries,list(n))
+	}
+	return(new_discoveries)
+}
 
-dr1 <- length(u1)/total_tries[1]
-dr2 <- length(setdiff(u2,u1))/total_tries[2] 
-dr3 <- length(setdiff(u3,c(u2,u1)))/total_tries[3]
-dr4 <- length(setdiff(u4,c(u3,u2,u1)))/total_tries[4]
-dr5 <- length(setdiff(u5,c(u4,u3,u2,u1)))/total_tries[5]
-dr6 <- length(setdiff(u6,c(u5,u4,u3,u2,u1)))/total_tries[6]
-dr7 <- length(setdiff(u7,c(u6,u5,u4,u3,u2,u1)))/total_tries[7]
-dr8 <- length(setdiff(u8,c(u7,u6,u5,u4,u3,u2,u1)))/total_tries[8]
-dr9 <- length(setdiff(u9,c(u8,u7,u6,u5,u4,u3,u2,u1)))/total_tries[9]
-dr10 <- length(setdiff(u10,c(u9,u8,u7,u6,u5,u4,u3,u2,u1)))/total_tries[10]
-dr11 <- length(setdiff(u11,c(u10,u9,u8,u7,u6,u5,u4,u3,u2,u1)))/total_tries[11]
+# example usage
+cumulative_novel_counts(sets)
 
-e1c_rates <- c(dr1,dr2,dr3,dr4,dr5,dr6,dr7,dr8,dr9,dr10,dr11)
+# the percentage of new systems discovered, out of the total discovery attempts in that iteration
+cumulative_discovery_rate <- function(df_name,system_type) {
+	new_discoveries <- cumulative_novel_counts(itersets(df_name,system_type))
+	tries <- itertries(df_name,system_type)
+	return(lengths(new_discoveries)/tries)
+}
 
+# plot percentage of tries that yielded a novel discovery per iteration
+rate_plot <- function(e1i,e1c,e2i,e2c,title) {
+	time <- seq(1,15)
+	plot(time,e2c,las=1,ylim=c(0,1),type="l",lty="dotted",col="red",ylab="proportion novel",xlab="iteration",main=title)
+	lines(e1i,type="l")
+	lines(e1c,type="l",lty="dotted")
+	lines(e2i,type="l",col="red")
+	legend(10, 1, cex=0.8, legend=c("Exp1 individual", "Exp1 cultural","Exp2 individual", "Exp2 cultural"), 
+		   col=c("black", "black","red", "red"), 
+		   lty=c("solid","dotted","solid","dotted"))
+}
 
-# Experiment 2, condition I
-df <- df2i
-total_tries <- table(df$iteration)  # number of tries per iteration, 
-u1 <- unique(subset(df,iteration==1)$system512)
-u2 <- unique(subset(df,iteration==2)$system512)
-u3 <- unique(subset(df,iteration==3)$system512)
-u4 <- unique(subset(df,iteration==4)$system512)
-u5 <- unique(subset(df,iteration==5)$system512)
-u6 <- unique(subset(df,iteration==6)$system512)
-u7 <- unique(subset(df,iteration==7)$system512)
-u8 <- unique(subset(df,iteration==8)$system512)
+################################################
+# Discovery rate per iteration - 512 system
+################################################
 
-dr1 <- length(u1)/total_tries[1]
-dr2 <- length(setdiff(u2,u1))/total_tries[2] # setdiff(a,b) returns what is in a and not in b
-dr3 <- length(setdiff(u3,c(u2,u1)))/total_tries[3]
-dr4 <- length(setdiff(u4,c(u3,u2,u1)))/total_tries[4]
-dr5 <- length(setdiff(u5,c(u4,u3,u2,u1)))/total_tries[5]
-dr6 <- length(setdiff(u6,c(u5,u4,u3,u2,u1)))/total_tries[6]
-dr7 <- length(setdiff(u7,c(u6,u5,u4,u3,u2,u1)))/total_tries[7]
-dr8 <- length(setdiff(u8,c(u7,u6,u5,u4,u3,u2,u1)))/total_tries[8]
+# Experiment 1
+e1i_512 <- cumulative_discovery_rate(df1i,512)
+e1c_512 <- cumulative_discovery_rate(df1c,512)
 
-e2i_rates <- c(dr1,dr2,dr3,dr4,dr5,dr6,dr7,dr8)
+# Experiment 2
+e2i_512 <- cumulative_discovery_rate(df2i,512)
+e2c_512 <- cumulative_discovery_rate(df2c,512)
 
+rate_plot(e1i_512,e1c_512,e2i_512,e2c_512,"discovery rate - 512 systems")
 
-# Experiment 2, condition C
-df <- df2c
-total_tries <- table(df$iteration)  # number of tries per iteration, 
-"  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 
-45 44 38 30 23 16 11  5  3  2  2  1  1  1  1"
-u1 <- unique(subset(df,iteration==1)$system512)
-u2 <- unique(subset(df,iteration==2)$system512)
-u3 <- unique(subset(df,iteration==3)$system512)
-u4 <- unique(subset(df,iteration==4)$system512)
-u5 <- unique(subset(df,iteration==5)$system512)
-u6 <- unique(subset(df,iteration==6)$system512)
-u7 <- unique(subset(df,iteration==7)$system512)
-u8 <- unique(subset(df,iteration==8)$system512)
-u9 <- unique(subset(df,iteration==9)$system512)
-u10 <- unique(subset(df,iteration==10)$system512)
-u11 <- unique(subset(df,iteration==11)$system512)
-u12 <- unique(subset(df,iteration==12)$system512)
-u13 <- unique(subset(df,iteration==13)$system512)
-u14 <- unique(subset(df,iteration==14)$system512)
-u15 <- unique(subset(df,iteration==15)$system512)
+################################################
+# Discovery rate per iteration - 1024 system
+################################################
 
-dr1 <- length(u1)/total_tries[1]
-dr2 <- length(setdiff(u2,u1))/total_tries[2] 
-dr3 <- length(setdiff(u3,c(u2,u1)))/total_tries[3]
-dr4 <- length(setdiff(u4,c(u3,u2,u1)))/total_tries[4]
-dr5 <- length(setdiff(u5,c(u4,u3,u2,u1)))/total_tries[5]
-dr6 <- length(setdiff(u6,c(u5,u4,u3,u2,u1)))/total_tries[6]
-dr7 <- length(setdiff(u7,c(u6,u5,u4,u3,u2,u1)))/total_tries[7]
-dr8 <- length(setdiff(u8,c(u7,u6,u5,u4,u3,u2,u1)))/total_tries[8]
-dr9 <- length(setdiff(u9,c(u8,u7,u6,u5,u4,u3,u2,u1)))/total_tries[9]
-dr10 <- length(setdiff(u10,c(u9,u8,u7,u6,u5,u4,u3,u2,u1)))/total_tries[10]
-dr11 <- length(setdiff(u11,c(u10,u9,u8,u7,u6,u5,u4,u3,u2,u1)))/total_tries[11]
-dr12 <- length(setdiff(u12,c(u11,u10,u9,u8,u7,u6,u5,u4,u3,u2,u1)))/total_tries[12]
-dr13 <- length(setdiff(u13,c(u12,u11,u10,u9,u8,u7,u6,u5,u4,u3,u2,u1)))/total_tries[13]
-dr14 <- length(setdiff(u14,c(u13,u12,u11,u10,u9,u8,u7,u6,u5,u4,u3,u2,u1)))/total_tries[14]
-dr15 <- length(setdiff(u15,c(u14,u13,u12,u11,u10,u9,u8,u7,u6,u5,u4,u3,u2,u1)))/total_tries[15]
+# Experiment 1
+e1i_1024 <- cumulative_discovery_rate(df1i,1024)
+e1c_1024 <- cumulative_discovery_rate(df1c,1024)
 
-e2c_rates <- c(dr1,dr2,dr3,dr4,dr5,dr6,dr7,dr8,dr9,dr10,dr11,dr12,dr13,dr14,dr15)
+# Experiment 2
+e2i_1024 <- cumulative_discovery_rate(df2i,1024)
+e2c_1024 <- cumulative_discovery_rate(df2c,1024)
 
-### PLOT ###
-# solid: individual   dashed: cultural
-# black: Exp1         blue:   Exp2
-time <- seq(1,15)
-plot(time,e2c_rates,las=1,ylim=c(0,1),type="l",lty="dotted",col="blue",main="discovery rate")
-lines(e1i_rates,type="l")
-lines(e1c_rates,type="l",lty="dotted")
-lines(e2i_rates,type="l",col="blue")
+rate_plot(e1i_1024,e1c_1024,e2i_1024,e2c_1024,"discovery rate - 1024 systems")
 
 
-##########################################
+##########################################################################
+# Proportion of the space discovered over time
+##########################################################################
+
+# Out of the 512 total systems, how many have been discovered by iteration x?
+# This is cumulative so iteration 5 contains all uniques discovered in iteration 1-5
+
+sets <- itersets(df1i,512)     # uniques within each iteration
+cumulative_novel_counts(sets)  # uniques
+
+lengths(cumulative_novel_counts(itersets(df1i,512)))
+
+space_discovery <- function(df_name,system_type) {
+	x <- lengths(cumulative_novel_counts(itersets(df_name,system_type)))
+	discovered <- c()  # cumulative discoveries by each iteration
+	for (i in 1:length(x)) {
+		discovered <- c(discovered,sum(x[1:i]))
+	}
+	return(discovered/system_type)
+}
+
+# example usage
+space_discovery(df1i,512)
+
+################################################
+# Expansion rate - 512 system
+################################################
+
+a <- space_discovery(df1i,512)
+b <- space_discovery(df1c,512)
+c <- space_discovery(df2i,512)
+d <- space_discovery(df2c,512)
+
+rate_plot(a,b,c,d,"expansion rate - 512 systems")
+
+################################################
+# Expansion rate - 1024 system
+################################################
+
+a <- space_discovery(df1i,1024)
+b <- space_discovery(df1c,1024)
+c <- space_discovery(df2i,1024)
+d <- space_discovery(df2c,1024)
+
+rate_plot(a,b,c,d,"expansion rate - 1024 systems")
+
+
+##########################################################################
 # Questions
+##########################################################################
 
 # how on earth are these discovery rates similar?
 # coz the I condition ones are searching more complex systems PLUS all the less complex ones
 
 
+
+
+##########################################################################
+# Sanity check for the 1024 systems
+##########################################################################
+
+# I want to stick to the 512 systems for all analyses
+# But let's make sure we get the same discovery rates for the 1024 systems.
+
+head(df1$system1024) # double check these read in correctly as strings
+
+# Experiment 1
+length(unique(df1$system1024))/nrow(df1)    # 237 / 642 = 0.369
+                                            # 188 / 642 = 0.293 <- for system512
+length(unique(df1i$system1024))/nrow(df1i)  # 197 / 435 = 0.453
+                                            # 159 / 435 = 0.366 <- for system512
+length(unique(df1c$system1024))/nrow(df1c)  #  91 / 207 = 0.440
+                                            #  75 / 207 = 0.362 <- for system512
+
+# 512 things to discover in 642 tries vs 1024 things to discover in 642 tries.
 
 
 
